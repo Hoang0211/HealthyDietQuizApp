b714d6d9133c6804c75e062b530bcf1696a3e7f0
Ulm Jevgeni <ajeul001@edu.xamk.fi>
2021-10-22 12:04:44 +0300

    added progress bar counter

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startBtn = document.getElementById("start-btn");
+const progressBarFull = document.getElementById("progress-bar-full");
 
 const startContainer = document.getElementById("start-container");
 const quizContainer = document.getElementById("quiz-container");
@@ -8,4 +9,10 @@ startBtn.addEventListener("click", startQuiz);
 function startQuiz() {
     startContainer.classList.add("hide");
     quizContainer.classList.remove("hide");
+
 }
+
+//Update progress bar
+//where 3 is current question and 14 is max question amount
+progressBarFull.style.width = `${( 3 / 14 ) * 100 }%`;
+
