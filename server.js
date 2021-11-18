baa3cf20641bb5eed70fc26c5565cb74625cab7b
NguyenHoang <n3verland0211@gmail.com>
2021-11-18 14:02:15 +0200

    Fixed server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 
+const port = 8080;
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -9,6 +11,6 @@ app.get("/", (req, res) => {
     res.render("pages/index");
 })
 
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log("Server is running on port 8080!");
 });
